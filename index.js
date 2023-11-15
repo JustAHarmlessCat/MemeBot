a1d768f0ba6e9307cfb58a07a0b0014083d7aa52
JustAHarmlessCat <sulubjarne@gmail.com>
2023-11-15 21:54:08 +0100

    added translator and made the code more efficient
    Co-authored-by: Jan <Jan20202020@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits, Collection} = require('discord.js');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
 require('dotenv').config();
@@ -17,64 +17,81 @@ const client = new Client({
 
 client.commands = new Collection();
 
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync(path.join(__dirname, 'commands')).filter(file => file.endsWith('.js'));
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
-        if ('data' in command && 'execute' in command) {
-            client.commands.set(command.data.name, command)
+    commandFiles.forEach(file => {
+        const command = require(`./commands/${file}`);
+        if (command.data && command.execute) {
+            client.commands.set(command.data.name, command);
             console.log(`[INFO] Loaded command ${command.data.name}`);
-        } else {
-            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
         }
-    }
+    });
 
     client.application.commands.set(client.commands.map(command => command.data));
     console.log(`[INFO] Registered ${client.commands.size} commands`);
 });
 
 client.on('interactionCreate', async (interaction) => {
-
-    const { commandName } = interaction;
-
+    if (!interaction.isCommand()) return;
+    const command = client.commands.get(interaction.commandName);
+    if (!command) return;
     try {
-        await client.commands.get(commandName).execute(interaction);
+        await command.execute(interaction);
     } catch (error) {
         console.error(error);
     }
 });
 
 client.on("messageCreate", async (message) => {
-    if (!message.author.bot) {
-        let exist = false
-        for (let i = 0; i < Object.keys(badwordsObject).length; i ++){
-            const word = Object.keys(badwordsObject)[i]
-            if (message.content.toLowerCase().includes(word)){
-                exist = true
-            }
+    // Ignore messages from bots or without content
+    if (message.author.bot || !message.content) return;
+
+    // Convert the message content to lower case for easier comparison
+    const content = message.content.toLowerCase();
+
+    // Check if the message contains any bad words
+    const badWords = Object.keys(badwordsObject);
+    const hasBadWord = badWords.some(word => content.includes(word));
+
+    // Check if the message contains any of the specified names
+    const names = ['obamna', 'obama', 'floppa'];
+    const name = names.find(name => content.toLowerCase().includes(name));
+
+    console.log(`Message: ${name}`);
+
+    // If the message contains a bad word or a specified name, handle it
+    if (hasBadWord || name) {
+        // If the name is 'floppa', reply with these messages
+        if (name === 'floppa') {
+            ['im better', 'im smarter', 'im stronger'].forEach(m => message.reply(m));
         }
-        if (message.content.toLowerCase().includes('floppa')) {
-            await message.reply('im better')
-            await message.reply('im smarter')
-            await message.reply('im stronger')
-        }
-        if (exist === true){
-            if (message.content.toLowerCase().includes("obamna")){
-                for (let b = 0; b < 5; b++){
-                    await message.reply('du dummer nuttensohn')
-                }
-            }
-            if (message.content.toLowerCase().includes("obama")){
-                for (let b = 0; b < 5; b++){
-                    await message.reply('du dummer nuttensohn')
-                }
+        // If the name is 'obamna' or 'obama', translate the message and check for bad words
+        else if (name) {
+            console.log("translating")
+            const res = await fetch("https://libretranslate.com/translate", {
+                method: "POST",
+                body: JSON.stringify({
+                    q: message.content,
+                    source: "de",
+                    target: "en",
+                    secret: "DG3D9LT",
+                    api_key: "",
+                }),
+                headers: { "Content-Type": "application/json", "Origin": "https://libretranslate.com" },
+            });
+
+            const resJson = await res.json();
+            const translatedText = resJson.translatedText;
+
+            // If the translated text contains a bad word, reply with this message
+            if (translatedText && badWords.some(word => translatedText.toLowerCase().includes(word))) {
+                Array(5).fill('du dummer nuttensohn').forEach(m => message.reply(m));
             }
         }
     }
 });
+
 client.login(process.env.DISCORD_TOKEN);
