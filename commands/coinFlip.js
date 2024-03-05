edadad1a304030db5b8a0e75c59e090238b403d1
JustAHarmlessCat <sulubjarne@gmail.com>
2024-03-05 10:20:12 +0100

    Fix typo in coinFlip command description

--- commands/coinFlip.js
+++ commands/coinFlip.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('flipacoin')
-        .setDescription("FLip a coin and get heads or tails"),
+        .setDescription("Flip a coin and get heads or tails"),
     async execute(interaction = new CommandInteraction()) {
         interaction.reply(Math.random() % 2 === 0 ? "Heads" : "Tails");
         return;
